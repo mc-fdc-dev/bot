3c29c93a4ddef3fe85c3d4b9dd7bba96e7eef640
mc_fdc <support@mc-fdc.com>
2022-07-29 14:59:31 +0900

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -1,17 +1,20 @@
 from discord.ext import commands
 import discord
 
-from motor import motor_asyncio as motor
+from aiomysql import create_pool
 
 class FdcBot(commands.Bot):
     def __init__(self, *args, **kwargs):
-        pass
+        self.mysql = kwargs.pop("mysql")
+        kwargs["command_prefix"] = self._command_prefix
+        self.prefixes = {}
+        super().__init__(*args, **kwargs)
 
     def _command_prefix(self, message: discord.Message) -> str:
-        pass
+        if message.guild.id in self.prefixes:
+            return self.prefixes[message.guild.id]
+        else:
+            return "fb."
 
     async def setup_hook(self) -> None:
-        self.db = motor.AsyncIOMotorClient("")["FdcBot"]
-
-    def get_collection(self, name: str):
-        return self.db[name]
+        self.pool = create_pool(**self.mysql)
