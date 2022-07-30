f42fbfc94000c13dd5d58aa25694dbf7cc1e7e5b
mc_fdc <support@mc-fdc.com>
2022-07-30 21:14:04 +0900

    Update main.py

--- cogs/main.py
+++ cogs/main.py
@@ -2,4 +2,9 @@ from core import Cog
 
 
 class Main(Cog):
-    pass
+    
+    @Cog.command()
+    async def prefix(self, ctx):
+        async with self.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute("INSERT INTO Prefix VALUES(%s, %s);", (
